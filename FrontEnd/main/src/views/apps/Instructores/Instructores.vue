7e39a9f8314cb26f1fb33e3e335ac75b5d7f4b41
Daniel <iramdaniel.zunigaagustin@utxicotepec.edu.mx>
2024-04-22 08:29:30 -0600

    Se agrego la carpeta que contiene los archivos front

---
+++ FrontEnd/main/src/views/apps/Instructores/Instructores.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// common components
+import BaseBreadcrumb from '@/components/shared-instructores/BaseBreadcrumb.vue';
+import EditableTable from '@/components/table-instructores/EditableTable.vue';
+// theme breadcrumb
+const page = ref({ title: 'Instructor' });
+
+const breadcrumbs = ref([
+    {
+        text: '2 Nuevos Instructores',
+        disabled: true,
+        href: '#'
+    }
+]);
+</script>
+
+<template>
+    <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
+    <v-card elevation="10">
+        <v-card-text>
+            <EditableTable />
+        </v-card-text>
+    </v-card>
+</template>
